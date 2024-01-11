a2e2b56414764c0375e7604f526ac00f35f0df88
Gavo9649 <gprecurato@gmail.com>
2024-01-11 13:35:11 -0500

    lockwheel cmd uses ps4

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -61,7 +61,7 @@ public class RobotContainer {
     new Trigger(() -> Pilot.getOptionsButton()).onTrue(new InstantCommand(() -> m_swerveSubsystem.zeroGyro())); //PS4 CONTROLLER
 
     
-    new Trigger(() -> Pilot.getTriangleButton()).whileTrue(new SwerveLockWheels(m_swerveSubsystem)); 
+    // new Trigger(() -> Pilot.getTriangleButton()).whileTrue(new SwerveLockWheels(m_swerveSubsystem)); 
   }
 
   private void configureNamedCommands(){
